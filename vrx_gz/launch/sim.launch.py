c1e80f9320ea563d86c0e0979e1a71471edd05ca
spacedoge2320 <spacedoge2320@gmail.com>
2023-04-15 23:55:54 +0900

    second

---
+++ vrx_gz/launch/sim.launch.py
@@ -0,0 +1,91 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_path
+
+import vrx_gz.launch
+from vrx_gz.model import Model
+
+
+def launch(context, *args, **kwargs):
+    config_file = LaunchConfiguration('config_file').perform(context)
+    world_name = LaunchConfiguration('world').perform(context)
+    sim_mode = LaunchConfiguration('sim_mode').perform(context)
+    robot = LaunchConfiguration('robot').perform(context)
+    headless = LaunchConfiguration('headless').perform(context).lower() == 'true'
+    robot_urdf = LaunchConfiguration('urdf').perform(context)
+
+    launch_processes = []
+
+    models = []
+    if config_file and config_file != '':
+        with open(config_file, 'r') as stream:
+            models = Model.FromConfig(stream)
+    else:
+        m = Model('wamv', 'wam-v', [8, 3, -0.1, 0, 0, 1])
+        if robot_urdf and robot_urdf != '':
+            m.set_urdf(robot_urdf)
+    models.append(m)
+
+    launch_processes.extend(vrx_gz.launch.simulation(world_name, headless))
+    launch_processes.extend(vrx_gz.launch.spawn(sim_mode, world_name, models, robot))
+
+    return launch_processes
+
+
+def generate_launch_description():
+    vrx_ws_dir = get_package_share_path('vrx_gz')
+    urdf_file_path = os.path.join(vrx_ws_dir, 'models', 'usv_2022', 'usv_2022.urdf')
+    return LaunchDescription([
+    # Launch Arguments
+        DeclareLaunchArgument(
+        'world',
+        default_value='pool',
+        description='Name of world'),
+        DeclareLaunchArgument(
+        'sim_mode',
+        default_value='full',
+        description='Simulation mode: "full", "sim", "bridge".'
+                        'full: spawns robot and launch ros_gz bridges, '
+                        'sim: spawns robot only, '
+                        'bridge: launch ros_gz bridges only.'),
+        DeclareLaunchArgument(
+            'bridge_competition_topics',
+            default_value='True',
+            description='True to bridge competition topics, False to disable bridge.'),
+        DeclareLaunchArgument(
+            'config_file',
+            default_value='',
+            description='YAML configuration file to spawn'),
+        DeclareLaunchArgument(
+            'robot',
+            default_value='',
+            description='Name of robot to spawn if specified. '),
+        DeclareLaunchArgument(
+            'headless',
+            default_value='False',
+            description='True to run simulation headless (no GUI). '),
+        DeclareLaunchArgument(
+            'urdf',
+            default_value=urdf_file_path,
+            description='URDF file of the wam-v model. '),
+
+        OpaqueFunction(function=launch),
+    ])
